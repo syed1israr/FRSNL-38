0d411dba1779c7562199432c1fa3d6e69ceac530
Syed israr <121382749+syed1israr@users.noreply.github.com>
2023-10-01 17:22:17 +0530

    Update script.js

--- Dice -game/script.js
+++ Dice -game/script.js
@@ -31,17 +31,12 @@ function rollDice() {
         currentPlayerScore = player2Score;
     }
 
-    // Update the dice image
+   
     const diceImage = document.querySelector('.img_box img');
     diceImage.src = diceImages[diceRoll];
 
-    // Check if a 1 is rolled
     const isOneRolled = diceRoll === 0;
-
-    // Check if the current score plus the player's total score is less than maxScore
     const isScoreBelowMax = currentScore + currentPlayerScore < maxScore;
-
-    // Check if the roll is not a 1 and the total score is below maxScore
     if (!isOneRolled && isScoreBelowMax) {
         currentScore += diceRoll + 1;
         if (currentPlayer === 1) {
@@ -50,15 +45,12 @@ function rollDice() {
             player2Current.textContent = currentScore;
         }
     } else {
-        // Switch to the other player if a 1 is rolled or maxScore is reached
         switchPlayer();
     }
 }
 
-// Event listener for the roll button
 rollButton.addEventListener('click', rollDice);
 
-// Function to switch to the other player's turn
 function switchPlayer() {
     currentScore = 0;
     if (currentPlayer === 1) {
@@ -72,26 +64,19 @@ function switchPlayer() {
         currentPlayer = 1;
         player2Current.textContent = '0';
     }
-
-    // Check if a player has won (reached maxScore)
     if (player1Score >= maxScore || player2Score >= maxScore) {
         alert(`Player ${currentPlayer} wins!`);
         resetGame();
     }
 }
-
-// Event listener for the "Hol'up!" button
-holdButton.addEventListener('click', () => {
-    // Add the current score to the player's total score
-    if (currentPlayer === 1) {
+function holup(){
+  if (currentPlayer === 1) {
         player1Score += currentScore;
         player1Total.textContent = player1Score;
     } else {
         player2Score += currentScore;
         player2Total.textContent = player2Score;
     }
-
-    // Check if a player has won (reached maxScore)
     if (player1Score >= maxScore || player2Score >= maxScore) {
         alert(`Player ${currentPlayer} wins!`);
         leftside.style.backgroundColor = "white";
@@ -103,9 +88,11 @@ holdButton.addEventListener('click', () => {
         rightside.style.backgroundColor = "rgb(146, 40, 40)";
         switchPlayer();
     }
-});
+}
 
-// Function to reset the game
+
+holdButton.addEventListener('click',holup);
+    
 function resetGame() {
     player1Score = 0;
     player2Score = 0;
